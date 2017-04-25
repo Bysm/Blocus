f836f43034156e2a1a125ec9457f061b2cde17df
schouver <jean-etienne.schouver@telecomnancy.net>
2017-04-25 16:07:12 +0200

    maj fonctionnel

--- Project/get-easy.sh
+++ Project/get-easy.sh
@@ -1,27 +1,26 @@
 #!/bin/bash
-# chemin du dossier courant et fichiet txt avec URL
-PATH=$(dirname "$0")
-PATH_LIST="${PATH}/urls.txt"
 
-#verification que le fichier urls.txt est present
-if [ ! -e "${PATH_LIST}" ] ; then
+WORKING_DIR=$(dirname "$0")
+URL_LIST_FILE="${WORKING_DIR}/urls.txt"
+
+
+if [ ! -e "${URL_LIST_FILE}" ] ; then
 	echo "Le fichier 'urls.txt' est absent."
 	exit 1
 fi
-#fonction pour retirer l'entete
-header(){
+
+remove_com(){
 	grep -v '^$\|^#' "$1" | sed 's/$/ /'| tr -d '\n'
 }
-# on retire l'entete
-URL_LIST=$(header "${PATH_LIST}")
-#recupere le contenu de chaque ligne dans un tableau
-EASYLIST_URL=(${URL_LIST}) 
-#boucle sur chaque ligne
-for URL in "${EASYLIST_URL[@]}"
+
+URL_LIST=$(remove_com "${URL_LIST_FILE}")
+
+EASYLIST_URL_LIST=(${URL_LIST}) # URL list as array to loop
+
+for URL in "${EASYLIST_URL_LIST[@]}"
 do
 	echo "Telechargement de la EasyList."
-	#telecharge le easylist.txt
-	wget -q --no-check-certificate -p ${PATH} "${URL}"
+	wget -q --no-check-certificate -p ${WORKING_DIR} "${URL}"
 
 done
 echo "EasyList à jour"
